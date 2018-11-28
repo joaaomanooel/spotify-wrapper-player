90813d67cd6c7a3388678def023d6fb5ed45ab32
João Manoel Neto <joaaomanooel@gmail.com>
2018-11-27 22:45:58 -0200

    initial test to album info

---
+++ src/AlbumInfo.js
@@ -0,0 +1,11 @@
+export default function renderAlbumInfo(data, element) {
+  const marckup = `
+      <img class="album-image" src="https://i.scdn.co/image/a13aadb368a956be48fe9089acb64333f0c0ca40" alt="">
+      <p class="album-title">Make Yourself</p>
+      <p class="album-artist">Incubus</p>
+      <p class="album-counter">13 Músicas</p>
+  `;
+  let divItem = element.innerHTML;
+  divItem = marckup;
+  return divItem;
+}
