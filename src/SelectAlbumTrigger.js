1dbf5f0b23a86ca77c92276d04a77f546903210f
João Manoel Neto <joaaomanooel@gmail.com>
2018-12-01 20:56:26 -0200

    add SelectAlbumTrigger

---
+++ src/SelectAlbumTrigger.js
@@ -0,0 +1,20 @@
+import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
+import spotify from './Spotify';
+
+const albumList = document.getElementById('album-list');
+const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-musics');
+
+function makeRequest(albumId) {
+  spotify.album.getAlbum(albumId)
+    .then(data => renderAlbumInfo(data, albumInfo))
+    .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+}
+
+export default function selectAlbumTrigger() {
+  albumList.addEventListener('click', (e) => {
+    const { target } = e;
+    makeRequest(target.getAttribute('data-album-id'));
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import renderAlbumInfo from './AlbumInfo';
-import renderAlbumTracks from './AlbumTracks';
-import spotify from './Spotify';
+import selectAlbumTrigger from './SelectAlbumTrigger';
 import searchEnterTrigger from './SpotifyTrigger';
 
-const album = spotify.album.getAlbum('6nFULR21EyYSzxPUr60S3a');
-const albumInfo = document.getElementById('album-info');
-const albumTracks = document.getElementById('album-musics');
-
 searchEnterTrigger();
-
-album
-  .then(data => renderAlbumInfo(data, albumInfo))
-  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+selectAlbumTrigger();
