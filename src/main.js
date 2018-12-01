e5325f5a83547fe7e0e4ff5a345cea8d430abf8c
João Manoel Neto <joaaomanooel@gmail.com>
2018-12-01 16:46:29 -0200

    add album info in layout

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import spotify from './Spotify';
 import renderAlbuns from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Emicida');
 const albumList = document.getElementById('album-list');
 
+const album = spotify.album.getAlbum('6nFULR21EyYSzxPUr60S3a');
+const albumInfo = document.getElementById('album-info');
+
 albums
   .then(data => renderAlbuns(data.albums.items, albumList));
+
+album
+  .then(data => renderAlbumInfo(data, albumInfo));
