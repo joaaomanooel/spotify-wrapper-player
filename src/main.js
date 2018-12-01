8df1588d3b897dc1a277e6fdc065da7e57471185
João Manoel Neto <joaaomanooel@gmail.com>
2018-12-01 18:12:03 -0200

    add tracks in layout

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import spotify from './Spotify';
-import renderAlbuns from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
+import renderAlbuns from './AlbumList';
+import renderAlbumTracks from './AlbumTracks';
+import spotify from './Spotify';
 
 const albums = spotify.search.albums('Emicida');
 const albumList = document.getElementById('album-list');
 
 const album = spotify.album.getAlbum('6nFULR21EyYSzxPUr60S3a');
 const albumInfo = document.getElementById('album-info');
-
+const albumTracks = document.getElementById('album-musics');
 albums
   .then(data => renderAlbuns(data.albums.items, albumList));
 
 album
-  .then(data => renderAlbumInfo(data, albumInfo));
+  .then(data => renderAlbumInfo(data, albumInfo))
+  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
