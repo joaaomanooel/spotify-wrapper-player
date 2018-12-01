dab8050f3b361d09be380d92311cfe45fe877b2a
João Manoel Neto <joaaomanooel@gmail.com>
2018-12-01 20:16:07 -0200

    add artist filter for  album list

---
+++ src/SpotifyTrigger.js
@@ -0,0 +1,14 @@
+import renderAlbuns from './AlbumList';
+import spotify from './Spotify';
+
+const albumList = document.getElementById('album-list');
+const searchInput = document.getElementById('search-input');
+const searchForm = document.getElementById('search-form');
+
+export default function searchEnterTrigger() {
+  searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    spotify.search.albums(searchInput.value)
+      .then(data => renderAlbuns(data.albums.items, albumList));
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import renderAlbumInfo from './AlbumInfo';
-import renderAlbuns from './AlbumList';
 import renderAlbumTracks from './AlbumTracks';
 import spotify from './Spotify';
-
-const albums = spotify.search.albums('Emicida');
-const albumList = document.getElementById('album-list');
+import searchEnterTrigger from './SpotifyTrigger';
 
 const album = spotify.album.getAlbum('6nFULR21EyYSzxPUr60S3a');
 const albumInfo = document.getElementById('album-info');
 const albumTracks = document.getElementById('album-musics');
-albums
-  .then(data => renderAlbuns(data.albums.items, albumList));
+
+searchEnterTrigger();
 
 album
   .then(data => renderAlbumInfo(data, albumInfo))
